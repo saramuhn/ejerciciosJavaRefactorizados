b9763e9e808fd77f6d32b7edb52c84355fb47e83
saramuhn <sara_munoz82151@elpoli.edu.co>
2019-12-11 09:48:54 -0500

    Refactorizado punto Persona

--- src/com/ejercicios/persona/EjecutableP.java
+++ src/com/ejercicios/persona/EjecutableP.java
@@ -7,58 +7,68 @@ public class EjecutableP {
         ejecutar();
     }
 
+    private static Scanner sc = new Scanner(System.in);
+
+
     public static void ejecutar() {
-        Scanner sc = new Scanner(System.in);
+        Persona personaEstandar = ingresoDatosCompletoPersona();
+        Persona personaParcial = ingresoDatosParcialesPersona();
+        Persona personaPorDefecto = ingresoDatosPorDefectoPersona();
 
-        String nombre;
-        int edad;
-        double peso;
-        double altura;
-        char sexo;
-        String primeraPersona;
-        String segundaPersona;
-        String terceraPersona;
-        System.out.print("Ingrese nombre de la primera Persona: ");
-        nombre = sc.nextLine();
-        System.out.print("Ingrese edad de la primera Persona: ");
-        edad = sc.nextInt();
-        System.out.print("Ingrese peso de la primera Persona: ");
-        peso = sc.nextDouble();
-        System.out.print("Ingrese altura de la primera Persona: ");
-        altura = sc.nextDouble();
-        System.out.print("Ingrese sexo de la primera Persona (M para Mujer y H para Hombre): ");
-        sexo = sc.next().charAt(0);
+        mostrarDatosPersona(personaEstandar);
+        calculoDatosPersona(personaEstandar);
 
-        Persona personaUno = new Persona(nombre, edad, peso, altura, sexo);
-        Persona personaDos = new Persona(nombre, edad, sexo);
-        Persona personaTres = new Persona();
+        mostrarDatosPersona(personaParcial);
+        calculoDatosPersona(personaParcial);
 
-        System.out.println("\n****Datos primera persona****");
-        primeraPersona = personaUno.toString();
-        System.out.println(primeraPersona);
-        personaUno.calcularPesoIdeal();
-        personaUno.esMayor();
-
-        System.out.println("\n****Datos segunda persona****");
-        segundaPersona = personaDos.toString();
-        System.out.println(segundaPersona);
-        personaDos.calcularPesoIdeal();
-        personaDos.esMayor();
-
-
-        System.out.println("\n****Datos tercera persona****");
-        personaTres.setNombre("Sara");
-        personaTres.setEdad(22);
-        personaTres.setAltura(1.60);
-        personaTres.setPeso(86.5);
-        personaTres.setSEXO('M');
-        terceraPersona = personaTres.toString();
-        System.out.println(terceraPersona);
-        personaTres.calcularPesoIdeal();
-        personaTres.esMayor();
-
-
+        mostrarDatosPersona(personaPorDefecto);
+        calculoDatosPersona(personaPorDefecto);
     }
+
+    private static Persona ingresoDatosCompletoPersona() {
+        System.out.print("Ingrese nombre de la primera Persona: ");
+        String nombre = sc.nextLine();
+        System.out.print("Ingrese edad de la primera Persona: ");
+        int edad = sc.nextInt();
+        System.out.print("Ingrese peso de la primera Persona: ");
+        double peso = sc.nextDouble();
+        System.out.print("Ingrese altura de la primera Persona: ");
+        double altura = sc.nextDouble();
+        System.out.print("Ingrese sexo de la primera Persona (M para Mujer y H para Hombre): ");
+        char sexo = sc.next().charAt(0);
+        Persona personaEstandar;
+        personaEstandar = new Persona(nombre, edad, peso, altura, sexo);
+        return personaEstandar;
+    }
+
+    public static void mostrarDatosPersona(Persona persona) {
+        String toStringPersona;
+        System.out.println("\n-----------------------");
+        toStringPersona = persona.toString();
+        System.out.println(toStringPersona);
+    }
+
+    public static void calculoDatosPersona(Persona persona) {
+        persona.calcularPesoIdeal();
+        persona.mostrarEsMayorDeEdad();
+    }
+
+    private static Persona ingresoDatosParcialesPersona() {
+        Persona personaParcial;
+        personaParcial = new Persona("Maria antonia", 2, 'M');
+        return personaParcial;
+    }
+
+    private static Persona ingresoDatosPorDefectoPersona() {
+        Persona personaPorDefecto = new Persona();
+        personaPorDefecto.setNombre("Sara");
+        personaPorDefecto.setEdad(22);
+        personaPorDefecto.setAltura(1.60);
+        personaPorDefecto.setPeso(86.5);
+        personaPorDefecto.setSexo('M');
+        return personaPorDefecto;
+    }
+
 
 }
 
