848672a77a5d7ae96204aa6842acbfc20943b589
saramuhn <sara_munoz82151@elpoli.edu.co>
2019-12-11 09:48:54 -0500

    Refactorizado punto Persona

--- src/com/ejercicios/persona/Persona.java
+++ src/com/ejercicios/persona/Persona.java
@@ -8,91 +8,106 @@ public class Persona {
     protected String dni;
     protected double peso = 0;
     protected double altura = 0;
-    protected char SEXO = 'H';
+    protected char sexo;
+    private Random r = new Random();
+
+    private static final char SEXO_POR_DEFECTO = 'H';
+    private static final int MAYORIA_EDAD = 18;
+    private static final char SEXO_MUJER = 'M';
+    private static final char SEXO_HOMBRE = 'H';
+    private static final int SOBREPESO = 1;
+    private static final int BAJO_PESO = -1;
+    private static final int PESO_IDEAL = 0;
+    private static final int IMC_SOBREPESO = 25;
+    private static final int IMC_BAJO_PESO = 20;
+
 
     public Persona() {
-        dni=generarDNI();
+        dni = generarDNI();
+        comprobarSexo();
     }
 
-    public Persona(String nombre, int edad, char SEXO) {
+    public Persona(String nombre, int edad, char sexo) {
         this.nombre = nombre;
         this.edad = edad;
-        this.SEXO = SEXO;
-        dni=generarDNI();
+        this.sexo = sexo;
+        dni = generarDNI();
+        comprobarSexo();
     }
 
-    public Persona(String nombre, int edad, double peso, double altura, char SEXO) {
+    public Persona(String nombre, int edad, double peso, double altura, char sexo) {
         this.nombre = nombre;
         this.edad = edad;
-        this.SEXO = SEXO;
+        this.sexo = sexo;
         this.peso = peso;
         this.altura = altura;
         this.dni = generarDNI();
+        comprobarSexo();
     }
 
     public double calcularImc() {
         double pesoIdeal = (peso / Math.pow(altura, 2));
-        if (pesoIdeal < 20)
-            pesoIdeal = -1;
-        if (pesoIdeal >= 20 && pesoIdeal <= 25)
-            pesoIdeal = 0;
-        if (pesoIdeal > 25)
-            pesoIdeal = 1;
+        if (pesoIdeal < IMC_BAJO_PESO)
+            pesoIdeal = BAJO_PESO;
+        if (pesoIdeal >= IMC_BAJO_PESO && pesoIdeal <= IMC_SOBREPESO)
+            pesoIdeal = PESO_IDEAL;
+        if (pesoIdeal > IMC_SOBREPESO)
+            pesoIdeal = SOBREPESO;
 
         return pesoIdeal;
     }
 
-    public void calcularPesoIdeal(){
-        double pesoIdeal= calcularImc();
-        if (pesoIdeal==-1)
+    public void calcularPesoIdeal() {
+        double pesoIdeal = calcularImc();
+        if (pesoIdeal == BAJO_PESO)
             System.out.println("Tiene Bajo de Peso");
-        else{
-            if (pesoIdeal==1)
-                System.out.println("Tiene Sobrepeso");
-            else
-                System.out.println("Tiene Peso ideal");
-        }
+        if (pesoIdeal == SOBREPESO)
+            System.out.println("Tiene Sobrepeso");
+        if (pesoIdeal == PESO_IDEAL)
+            System.out.println("Tiene Peso ideal");
+
     }
 
     public boolean esMayorDeEdad() {
-        if (edad >= 18)
-            return true;
+        boolean mayor;
+        if (edad >= MAYORIA_EDAD)
+            mayor = true;
         else
-            return false;
+            mayor = false;
+        return mayor;
     }
 
-    public void esMayor(){
-        boolean mayor= esMayorDeEdad();
-        if(mayor=true)
+    public void mostrarEsMayorDeEdad() {
+        boolean mayor = esMayorDeEdad();
+        if (mayor)
             System.out.println("Es mayor de edad");
         else
             System.out.println("Es menor de edad");
     }
 
-    private void comprobarSexo(char sexo) {
-        if (SEXO == 'M')
-            SEXO = 'M';
+    private void comprobarSexo() {
+        if (sexo == SEXO_MUJER)
+            this.sexo = SEXO_MUJER;
         else {
-            if (SEXO != 'H')
-                SEXO = 'H';
+            if (sexo != SEXO_HOMBRE)
+                this.sexo = SEXO_POR_DEFECTO;
         }
     }
 
     @Override
     public String toString() {
         return "Nombre:" + nombre + "\tEdad:" + edad +
-                "\tDNI:" + dni  +
+                "\tDNI:" + dni +
                 "\tPeso:" + peso +
                 "\tAltura:" + altura +
-                "\tSexo:" + SEXO;
+                "\tSexo:" + sexo;
     }
 
     private String generarDNI() {
-        Random r = new Random();
         int dniAleatorio = r.nextInt(90000000) + 10000000;
         String letraDNI;
         letraDNI = Character.toString(generarLetraDNI(dniAleatorio));
-        dni= String.valueOf(dniAleatorio)+"-"+letraDNI;
+        dni = String.valueOf(dniAleatorio) + "-" + letraDNI;
         return dni;
     }
 
@@ -118,8 +133,9 @@ public class Persona {
         this.altura = altura;
     }
 
-    public void setSEXO(char SEXO) {
-        this.SEXO = SEXO;
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+        comprobarSexo();
     }
 
 }
